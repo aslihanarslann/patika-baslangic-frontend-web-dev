bca3ae2868b8943e7d9ee219035bcc28a92a7c58
Aslıhan Arslan <arslan.asli2230@gmail.com>
2025-08-09 01:42:00 +0300

    bootstrap odev1 and js-bootstrap lessons

---
+++ js-kategorisi/variables/007-template-literals-kullanimi.js
@@ -0,0 +1,21 @@
+// ********** Template Literals Kullanimi ********** 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+// let userName = "hakan"
+// const DOMAIN = "kodluyoruz.org"
+
+// let email = userName + "@" + DOMAIN
+
+// console.log("Merhaba", userName, "sitemize hosgeldin", "mail adresin:", email)
+
+// let info = `
+// Merhaba ${userName} sitemize hosgeldin..
+// mail adresin: ${email}
+
+// mail adresinin uzunlugu: ${email.length}
+// borcunuz: ${(2+3)*10} TL
+// gunun saat bilgisi: ${new Date().getHours()}
+// kisa isminiz: ${userName[0]}.
+// `
+
+// console.log(info)
